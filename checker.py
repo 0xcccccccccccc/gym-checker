c61fd9136385bdde6c68e5fbae9b9c6b323631a1
0xcccccccccccc <0xcccc@bupt.edu.cn>
2020-10-09 17:17:27 +0800

    checker.py ADDED

---
+++ checker.py
@@ -0,0 +1,18 @@
+import requests,json,datetime
+
+session=requests.Session()
+session.get("https://reservation.bupt.edu.cn/index.php/1600")
+now=datetime.datetime.now()+datetime.timedelta(hours=8)
+datestr=datetime.datetime.strftime(now,"%Y%m%d")
+resp=session.get("https://reservation.bupt.edu.cn/index.php/API/Room/ajax_return_one_period_one_room_timestate?room_id=15415&datesList%5B%5D="+datestr)
+decoder=json.JSONDecoder()
+data=decoder.decode(resp.text)
+
+
+max_people=list(map(int,list(data[datestr]['max_people'].values())))
+already_reserve=list(map(int,list(data[datestr]['already_reserve'].values())))
+for ind in range(len(max_people)):
+    if max_people[ind]-already_reserve[ind]!=0:
+    	raise Exception("New Appointment Avaliable Now!")
+
+
